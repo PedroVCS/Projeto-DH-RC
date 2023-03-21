af3fd6f8fd18b75284f00f2399ef69bffce0c32d
PedroVCS <pedro_victor61@hotmail.com>
2023-03-21 00:54:50 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="wrapper">
+      <aside>
+        <ul>
+          <li> 
+            <a href="">
+            <i className='fas fa-fw fa-tachometer-alt'></i>
+              <span>
+                  Dashbord -DH Movies
+              </span>
+            </a>
+          </li>
+          <li>
+              <a href=""> </a>
+          </li>
+          <li>
+              <a href="/"></a>
+              <i className=''></i>
+          </li>
+          <li>
+              <a href=""></a>
+          </li>
+          </ul>
+      </aside>
+    </div>
+  );
+}
+
+export default App;
